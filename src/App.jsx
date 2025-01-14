3c12b47245f5da70868af5ffa280f43a57c1d3ff
Debugger Sage <191020451+DebuggerSage@users.noreply.github.com>
2025-01-14 13:30:49 -0800

    03-10

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png'
+import componentsImg from './assets/components.png'
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -6,7 +7,7 @@ function genRandomInt(max) {
 }
 
 function Header() {
-  const description = reactDescriptions[genRandomInt(reactDescriptions.length-1)];
+  const description = reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
   return (
     <header>
       <img src={reactImg} alt="Stylized atom" />
@@ -18,12 +19,34 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title}/>
+      <h3>{props.title}</h3>
+      <p>{props.descriptions}</p>
+    </li>
+  )
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+              title="Components" 
+              descriptions="The core UI building block." 
+              image={componentsImg} 
+              />
+            <CoreConcept title="Props" />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
       </main>
     </div>
   );
