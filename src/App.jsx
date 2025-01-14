1f4139c78911d220795181f8aec7d433552b83ad
Debugger Sage <191020451+DebuggerSage@users.noreply.github.com>
2025-01-14 13:15:46 -0800

    03-08-09

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import reactCoreConcepts from './assets/react-core-concepts.png'
+import reactImg from './assets/react-core-concepts.png'
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -6,12 +6,13 @@ function genRandomInt(max) {
 }
 
 function Header() {
+  const description = reactDescriptions[genRandomInt(reactDescriptions.length-1)];
   return (
     <header>
-      <img src={reactCoreConcepts} alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are going to build!
+        {description} React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
