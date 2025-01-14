ce1ac3febba938532bf12ab5de6157929c63d322
Debugger Sage <191020451+DebuggerSage@users.noreply.github.com>
2025-01-14 13:55:26 -0800

    03-11 props with destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import reactImg from './assets/react-core-concepts.png'
-import componentsImg from './assets/components.png'
+
+import { CORE_CONCEPTS } from './data'
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -19,12 +21,22 @@ function Header() {
   );
 }
 
-function CoreConcept(props) {
+// function CoreConcept(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title}/>
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
+//     </li>
+//   )
+// }
+// OBJECT DESTRUCTURING 
+function CoreConcept({title, image, description}) {
   return (
     <li>
-      <img src={props.image} alt={props.title}/>
-      <h3>{props.title}</h3>
-      <p>{props.descriptions}</p>
+      <img src={image} alt={title}/>
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   )
 }
@@ -38,13 +50,13 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept 
-              title="Components" 
-              descriptions="The core UI building block." 
-              image={componentsImg} 
+              title={CORE_CONCEPTS[0].title} 
+              description={CORE_CONCEPTS[0].description} 
+              image={CORE_CONCEPTS[0].image}
               />
-            <CoreConcept title="Props" />
-            <CoreConcept />
-            <CoreConcept />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
