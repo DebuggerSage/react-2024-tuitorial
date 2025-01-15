ed50ac8e541fd5d17a0cb8e2c6cbcd927e7970dc
Debugger Sage <191020451+DebuggerSage@users.noreply.github.com>
2025-01-15 09:16:29 -0800

    04-10. Setting Default Prop Values

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -31,7 +31,6 @@ export default function Examples() {
         <Section id="examples" title="Examples">
           <Tabs 
           // ButtonContainer={Section} // custom component
-          ButtonContainer="menu"
           buttons={
             <>
               <TabButton
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,4 +1,4 @@
-export default function Tabs({children, buttons, ButtonContainer}) {
+export default function Tabs({children, buttons, ButtonContainer='menu'}) {
     // const ButtonContainer = buttonContainer; // if the props were defined as 'buttonContainer'
     return <>
     <ButtonContainer>{buttons}</ButtonContainer>
